34d09afa8001adab150d5113c2302f587841646f
Benko3 <152974533+Benko3@users.noreply.github.com>
2024-01-16 13:05:19 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,32 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string sVelika, sMala, sPrvaTri, sZadnjihPet, s8_11;
-            Console.WriteLine("Unesite recenicu:");
-            sVelika = Console.ReadLine();
-            Console.WriteLine("Ispis velikim slovima: {0}, " +
-            "\nIspis malim slovima: {1}",
-            sVelika.ToUpper(), sVelika.ToLower());
-            
-            Console.WriteLine("Unesite prva tri znaka");
-            sPrvaTri = Console.ReadLine();
-            Console.WriteLine(sPrvaTri.Substring(0, 3));
-            Console.WriteLine(sPrvaTri);
-            Console.WriteLine("Unesite zadnjih pet znakova:");
-            sZadnjihPet = Console.ReadLine();
-            Console.WriteLine(sZadnjihPet.Substring(0, 5));
-            Console.WriteLine("Unesite znakove od 8 do 11:");
-            s8_11 = Console.ReadLine();
-            Console.WriteLine(s8_11.Substring(s8_11.Length - 1, 3));
+            string s1 = "Hrvatska ima more";
+            string s2 = "Italija je prvak svijeta";
+            string s3 = s1.Substring(0, 7) + s2.Substring(6);
+            Console.WriteLine(s3);
             Console.ReadLine();
-
-
         }
+        
     }
 }
